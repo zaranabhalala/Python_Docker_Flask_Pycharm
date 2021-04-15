306426d458ac9561c3bad47244cd8d6bbe7fedd1
zaranabhalala <zaranabhalala1996@gmail.com>
2021-04-15 16:55:15 -0400

    Feature: Updated app.py for templates

--- app/app.py
+++ app/app.py
@@ -25,9 +25,16 @@ def cities_import() -> List[Dict]:
 
     return result
 
-
 @app.route('/')
-def index() -> str:
+def index():
+    user = {'username': 'Miguel'}
+    cities_data = cities_import()
+
+    return render_template('index.html', title='Home', user=user, cities=cities_data)
+
+
+@app.route('/api/cities')
+def cities() -> str:
     js = json.dumps(cities_import())
     resp = Response(js, status=200, mimetype='application/json')
     return resp
